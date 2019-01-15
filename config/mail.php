e41b9c8c941daa4fd358a26bf37ed9f6b48e20b6
Nipuna Weerasekara <w.nipuna@gmail.com>
2019-01-15 06:56:51 +0530

    Minor edit

--- config/mail.php
+++ config/mail.php
@@ -57,7 +57,7 @@ return [
 
     'from' => [
         'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Administrator @ Niweera TVDB'),
+        'name' => env('MAIL_FROM_NAME', 'Admin @ Niweera TVDB'),
     ],
 
     /*
